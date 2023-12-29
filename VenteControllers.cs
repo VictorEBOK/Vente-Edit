3a31f732c37929c4ac3aa497f63fd0298d60a0ce
VictorEBOK <147911307+VictorEBOK@users.noreply.github.com>
2023-12-29 21:34:34 +0100

    Add files via upload

---
+++ VenteControllers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VikShop.Models;
+
+namespace VikShop.Controllers;
+
+public class VenteController : Controller
+{
+    private readonly ILogger<VenteController> _logger;
+
+    public VenteController(ILogger<VenteController> logger)
+    {
+        _logger = logger;
+    }
+
+  
+
+
+    public IActionResult Edit(int id = -1)
+    {
+       
+        var model = new VenteEditViewModel();
+
+        if (id != -1)
+        {
+            model = new VenteEditViewModel()
+            {
+                SamplingDate = new DateTime(2023, 10, 30, 12, 54, 30),
+                SampleNumber = "AD19888",
+                NomDuVendeur = "Victor",
+                Categorie = "Robe",
+                Model = "Ensemble",
+                Prix = "50 Euro",
+                Commentaire = "je valide cette commande manuelle",
+                Id = id,
+            };
+        }
+
+        return View(model);
+    }
+
+
+    [HttpPost]
+    public IActionResult Edit(VenteEditViewModel model)
+    {
+        return View();
+    }
+}
+
---
+++ VenteEditViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using VikShop.Controllers;
+namespace VikShop.Models;
+public class VenteEditViewModel
+
+
+
+{
+    public int Id { get; set; }
+    [StringLength(8, ErrorMessage = "Le n° la reffence de l'article doit faire entre 4 et 8 caracteres maximum,", MinimumLength = 4)]
+
+    public string SampleNumber { get; set; }
+    public DateTime SamplingDate { get; set; }
+    public string NomDuVendeur { get; set; }
+    public string Categorie {get; set;}
+    public string Model { get; set; }
+    public string Prix { get; set; }
+    public string Commentaire { get; set; }
+
+    
+}
+
